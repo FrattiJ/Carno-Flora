ddae9b6db213ff0268a332a95d268c5dbf0c75eb
Jacob Fratti <JacobRFratti@gmail.com>
2024-02-01 21:54:50 -0500

    Set up react, tailwind, and basic boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+      <div>
+        <Navbar />
+        <Main />
+        <Footer />
+      </div>
+  )
+}
+
+export default App
