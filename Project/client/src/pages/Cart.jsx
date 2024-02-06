dec3d7b3be2d2c7b96cf88fc22be8e7026655e54
Ryan Caltabiano <rcaltabiano90@gmail.com>
2024-02-05 20:27:28 -0500

    Added router functionality and public folder for images

---
+++ Project/client/src/pages/Cart.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Cart() {
+    return <div><h1>Cart</h1>
+    <p>Cart content and data here</p></div>;
+}
---
+++ Project/client/src/pages/Shop.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Shop() {
+    return <div><h1>Shop</h1>
+    <p>Shop content and data here</p></div>;
+}
