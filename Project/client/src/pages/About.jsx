4fd563566636e816010371d43bb3111a55a71a7c
Jacob Fratti <JacobRFratti@gmail.com>
2024-02-05 21:22:43 -0500

    Added basic jsx framework for pages

--- Project/client/src/pages/About.jsx
+++ Project/client/src/pages/About.jsx
@@ -1,11 +1,35 @@
-import React from "react"; 
+import React from 'react'
 
-export default function About() {
-    return <div>
-        <h1 className="text-3xl font-bold py-6">About Us</h1>
-    <p className="text-xl py-6">
-        We are a company which offers a medium for plant enthusiasts to connect and sell their plant material.
-        We offer a secure method of transaction for customers so they can buy in confidence.
-    </p>
-    </div>;
+export default function About () {
+  return (
+    <div>
+        <div>
+            <h1>About Us</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda odio voluptatum ullam at earum temporibus? Unde dolorum quas nulla! Quaerat, quis. Dolorum, expedita fuga sed necessitatibus ducimus fugit repellat voluptates.</p>
+        </div>
+        <div>
+            <h2>Contact Us</h2>
+            <form action="">
+                <div className='grid md:grid-col-2 gap-4 w-full py-2'>
+                    <div className='flex flex-col'>
+                        <input type="text"  name='Full name' placeholder='Full Name' className='border-2 rounded-lg p-3 flex border-gray-300'/>
+                    </div>
+                    <div className='flex flex-col'>
+                        <input type="Email" name='Email' placeholder='Email' className='border-2 rounded-lg p-3 flex border-gray-300'/>
+                    </div>
+                    <div className='flex flex-col'>
+                        <input type="text" name='Phone Number' placeholder='Phone Number' className='border-2 rounded-lg p-3 flex border-gray-300'/>
+                    </div>
+                    <div className='flex flex-col'>
+                        <input type="text" name='Subject' placeholder='Subject' className='border-2 rounded-lg p-3 flex border-gray-300'/>
+                    </div>
+                    <div className='flex flex-col'>
+                        <textarea rows={10} name='Message' placeholder='Message' className='border-2 rounded-lg p-3 flex border-gray-300'/>
+                    </div>
+                    <input type="submit" value="Submit" className='border-2 rounded-lg p-3 border-gray-300 cursor-pointer' />
+                </div>
+            </form>
+        </div>
+    </div>
+  )
 }
--- Project/client/src/pages/Cart.jsx
+++ Project/client/src/pages/Cart.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 export default function Cart() {
-    return <div><h1>Cart</h1>
-    <p>Cart content and data here</p></div>;
+    return (
+    <div>
+        <h1>Cart</h1>
+        <div>
+            <img src="" alt="PLANT NAME" />
+            <p>Plant Name</p>
+            <p>Price</p>
+        </div>
+        <p>Total Price</p>
+        <p>Tax</p>
+        <p>Shipping</p>
+        <form action="">
+            {/* Payment Form */}
+        </form>
+        <button>Checkout</button>
+    </div>
+    );
 }
