5f7c546827053220e48125a6565b135e5f46360d
Jacob Fratti <JacobRFratti@gmail.com>
2024-02-07 19:11:53 -0500

    Login functionality added

--- Project/client/src/App.jsx
+++ Project/client/src/App.jsx
@@ -1,11 +1,39 @@
-// import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
-function App() {
+// import { StoreProvider } from './utils/GlobalState';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
   return (
+    <ApolloProvider client={client}>
       <div>
         <Navbar />
         {/* <Main /> */}
@@ -14,6 +42,7 @@ function App() {
         </main>
         <Footer />
       </div>
+      </ApolloProvider>
   )
 }
 
