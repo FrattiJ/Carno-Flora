5c20e5f3454d61f3d1f66bf272d1a888f217a2f4
cliffordrouzeau <142095138+cliffordrouzeau@users.noreply.github.com>
2024-02-07 20:11:24 -0500

    adding connection and models

---
+++ Project/server/models/Items.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose
+
+const item = new Schema({
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      min: 0,
+      default: 0
+    },
+    image: {
+        type: String
+      },
+  });
+  
+  const items = mongoose.model('items', item);
+  
+  module.exports = items;
